fa4a04ac8c07bc77cdbbbf982b59f634143de0f8
MirZhou <mir3732@live.com>
2020-05-29 11:47:46 +0800

    demo修改

--- src/pages/system/RoleAuthorizate.vue
+++ src/pages/system/RoleAuthorizate.vue
@@ -1,116 +1,69 @@
 <template>
-  <div>
-    <h2>TreeGrid CheckBox</h2>
-    <TreeGrid style="height:250px" :data="data" :checkbox="true" idField="id" treeField="name">
-      <GridColumn field="name" title="Name"></GridColumn>
-      <GridColumn field="size" title="Size"></GridColumn>
-      <GridColumn field="date" title="Date"></GridColumn>
+  <div class="tree-grid">
+    <TreeGrid :data="data" idField="id" treeField="menuName" :fit="true" :checkbox="true">
+      <GridColumn field="menuName" title="菜单名称"></GridColumn>
+      <GridColumn field="menuCode" title="菜单代码"></GridColumn>
+      <GridColumn field="dataPermission" title="数据权限">
+        <template slot="cell" slot-scope="{row}" v-if="row.id != 0">
+          <label>
+            <input
+              v-bind:name="'dataPermission' + row.id"
+              type="radio"
+              value="1"
+              v-model="row.dataPermission"
+            />可见本人
+          </label>
+          <label>
+            <input
+              v-bind:name="'dataPermission' + row.id"
+              type="radio"
+              value="2"
+              v-model="row.dataPermission"
+            />可见本部门
+          </label>
+          <label>
+            <input
+              v-bind:name="'dataPermission' + row.id"
+              type="radio"
+              value="3"
+              v-model="row.dataPermission"
+            />可见所有人
+          </label>
+        </template>
+      </GridColumn>
     </TreeGrid>
   </div>
 </template>
  
 <script>
+// 导入菜单JSON数据
+import menuData from "../../assets/json/menu.json";
+
 export default {
   data() {
     return {
-      data: this.getData()
-    };
-  },
-  methods: {
-    getData() {
-      return [
+      data: [
         {
-          id: 1,
-          name: "C",
-          size: "",
-          date: "02/19/2010",
-          children: [
-            {
-              id: 2,
-              name: "Program Files",
-              size: "120 MB",
-              date: "03/20/2010",
-              children: [
-                {
-                  id: 21,
-                  name: "Java",
-                  size: "",
-                  date: "01/13/2010",
-                  state: "closed",
-                  children: [
-                    {
-                      id: 211,
-                      name: "java.exe",
-                      size: "142 KB",
-                      date: "01/13/2010"
-                    },
-                    {
-                      id: 212,
-                      name: "jawt.dll",
-                      size: "5 KB",
-                      date: "01/13/2010"
-                    }
-                  ]
-                },
-                {
-                  id: 22,
-                  name: "MySQL",
-                  size: "",
-                  date: "01/13/2010",
-                  state: "closed",
-                  children: [
-                    {
-                      id: 221,
-                      name: "my.ini",
-                      size: "10 KB",
-                      date: "02/26/2009"
-                    },
-                    {
-                      id: 222,
-                      name: "my-huge.ini",
-                      size: "5 KB",
-                      date: "02/26/2009"
-                    },
-                    {
-                      id: 223,
-                      name: "my-large.ini",
-                      size: "5 KB",
-                      date: "02/26/2009"
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              id: 3,
-              name: "eclipse",
-              size: "",
-              date: "01/20/2010",
-              children: [
-                {
-                  id: 31,
-                  name: "eclipse.exe",
-                  size: "56 KB",
-                  date: "05/19/2009"
-                },
-                {
-                  id: 32,
-                  name: "eclipse.ini",
-                  size: "1 KB",
-                  date: "04/20/2010"
-                },
-                {
-                  id: 33,
-                  name: "notice.html",
-                  size: "7 KB",
-                  date: "03/17/2005"
-                }
-              ]
-            }
-          ]
+          id: 0,
+          menuName: "全部",
+          menuCode: "--",
+          children: menuData
         }
-      ];
-    }
+      ]
+    };
   }
 };
 </script>
+
+<style lang="stylus">
+.tree-grid {
+  margin-top: 6em;
+
+  input {
+    &[type=radio] {
+      width: unset;
+      display: inline;
+    }
+  }
+}
+</style>
